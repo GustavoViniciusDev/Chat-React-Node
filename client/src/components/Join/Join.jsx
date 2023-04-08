c3cc63cd729f9f8525c6f61eb86110b7e9875f4f
GustavoViniciusDev <gustavo.rocco.02@gmail.com>
2023-04-07 22:05:53 -0300

    Sem ideia para nome do commit

--- client/src/components/Join/Join.jsx
+++ client/src/components/Join/Join.jsx
@@ -1,14 +1,16 @@
 import React,{useRef} from 'react'
 import io from 'socket.io-client'
 
-export default function Join({setChatVisibility}) {
+export default function Join({setChatVisibility, setSocket}) {
 
     const usernameRef = useRef()
 
-    const handleSubmit = () => {
+    const handleSubmit = async () => {
         const username = usernameRef.current.value
         if(!username.trim()) return 
-        const socket =  io.connect('http://localhost:3001')
+        const socket = await io.connect('http://localhost:3001')
+        socket.emit('set_username', username)
+        setSocket(socket)
         setChatVisibility(true)
     }
 
